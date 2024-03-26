81fbdc311f7156697e595d5f47b3d811f4068583
Handi Priyono <handimacbook@gmail.com>
2024-03-26 20:54:05 +0700

    feat: init configs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import Navigation from "./app/navigation/index";
+import { useLoginContext, LoginContextWrap } from "./app/context/login";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import useCart from "./app/commons/stores/cart";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import * as SQLite from "expo-sqlite";
+import { useEffect } from "react";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
   },
 });
+export default function App() {
+  const data = useLoginContext();
+  const cart = useCart((state) => state.cart);
+
+  useEffect(() => {
+    const db = SQLite.openDatabase("db.db");
+    db.transaction((tx) => {
+      tx.executeSql(
+        "create table if not exists carts (id integer primary key not null, itemId integer);"
+      );
+      tx.executeSql(
+        "create table if not exists items (id integer primary key not null, title string, price integer, image string);"
+      );
+    });
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <LoginContextWrap.Provider value={{ ...data, cart }}>
+        <Navigation />
+      </LoginContextWrap.Provider>
+    </QueryClientProvider>
+  );
+}
